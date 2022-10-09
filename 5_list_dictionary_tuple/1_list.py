580bb9a1a2b016614ea315b7f028ce453ae11c10
hasanmehedi9034 <mehedi90344@gmail.com>
2022-10-09 23:31:41 +0600

    list

---
+++ 5_list_dictionary_tuple/1_list.py
@@ -0,0 +1,42 @@
+numbers = [12, 45, 65, 23, 89, 78, 11]\
+
+# print(numbers[1])
+# print(numbers[-6])
+
+# print(numbers)
+# print(numbers[1 : 5])
+
+# print(numbers[3 : ])
+
+# print(numbers[2 : 5: 2])
+
+# print(numbers[5: 2: -1])
+
+# print(numbers[-2 : -8: -2])
+
+# print(numbers[:])
+# print(numbers[: : -1])
+
+# numbers.clear()
+
+# print(numbers)
+
+numbers[len(numbers) : ] = [500]
+
+numbers.insert(0, 3)
+numbers.insert(0, 3)
+
+print(numbers)
+
+numbers.remove(3)
+
+# del numbers[:]
+
+numbers.sort()
+
+print(numbers)
+
+numbers.append(0)
+
+numbers.sort()
+print(numbers)
