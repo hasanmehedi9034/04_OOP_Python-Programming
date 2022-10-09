89880462efdfc7d700e0ba0538b2283661aeb5e9
hasanmehedi9034 <mehedi90344@gmail.com>
2022-10-09 23:49:01 +0600

    set and tuple

---
+++ 5_list_dictionary_tuple/2_set_tuple.py
@@ -0,0 +1,18 @@
+numbers = [12, 45, 56, 45, 12]
+print(numbers)
+
+nums = {12, 45, 56, 45, 12, 89}
+# print(sorted(nums))
+# print(nums)
+
+numbers_set = set(numbers)
+# print(numbers_set)
+
+tuple_ = (1, 3, 4, 5)
+# print(tuple_)
+
+tuple_2d = ([12, 45, 12], [45, 11])
+tupe
+
+
+
