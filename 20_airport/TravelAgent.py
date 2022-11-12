865d02ab8061e10e85e0ca6341da682cb5457664
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-12 23:13:33 +0600

    travel agent

---
+++ 20_airport/TravelAgent.py
@@ -0,0 +1,17 @@
+class TravelAgent:
+    def __init__(self, name):
+        self.name = name
+        self.trips = None
+    
+    def set_trip_one_city_one_way(self):
+        pass
+    
+    def set_trip_one_city_two_way(self):
+        pass
+    
+    def set_trip_multi_city_one_way(self):
+        pass
+    
+    def __repr__(self):
+        return f'TravelAgent: {self.name}'  
+    
