366a18a2648fe8403bc2932a1e05f1a9b4aa0152
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-17 12:04:55 +0600

    UML diagram primary

---
+++ 24_UML_diagram/2_basic_part.py
@@ -0,0 +1,10 @@
+'''
+    1. class diagram
+        1. inheritance -|>
+        2. association -
+        3. aggregation -^
+        4. composition
+        
+google class diagram multiplicity
+                
+'''
---
+++ 24_UML_diagram/6_sequence_diagram.py
@@ -0,0 +1,9 @@
+'''
+website (visual paradigm)
+
+1. class diagram 
+2. sequence diagram
+3. use case diagram
+4. activity diagram
+5. 
+'''
