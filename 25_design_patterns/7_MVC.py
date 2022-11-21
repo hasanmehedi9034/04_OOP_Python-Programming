21ccbb8509f2df94965598e7f52a09e6f90dbcfa
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-21 13:18:27 +0600

    architecture pattern(MVC)

---
+++ 25_design_patterns/7_MVC.py
@@ -0,0 +1,15 @@
+# google (model view controller)
+# architecture diagram (google)
+
+class Model:
+    def __init__(self):
+        pass
+    
+class ProductModel:
+    pass
+
+class View:
+    pass
+
+class Controller:
+    pass
