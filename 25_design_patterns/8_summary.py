517a2ba4eecf7b9995b59af893a3b50fe6b825d0
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-21 13:25:48 +0600

    design pattern

---
+++ 25_design_patterns/8_summary.py
@@ -0,0 +1,15 @@
+'''
+1. design pattern
+2. Architecture Pattern
+    1. MVCC pattern
+    2. Microservice
+    3. 3-tier
+3. Architecture Diagram
+
+python desing pattern(google)
+    1. python pattern guide
+    2. refactoring guru
+    3. geeksforgeeks
+    4. toptul
+    5. github(recomanded)
+'''
