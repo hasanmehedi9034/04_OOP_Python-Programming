ecdb83c3bdd9d66acf19b4da67d5ae36ae0fce2e
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-22 14:55:58 +0600

    delete data

---
+++ 26_lav_07_api/delete.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+
+#init flask app
+app = Flask(__name__)
+
+database = {
+    'minhaj': '100'
+}
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'Welcom to cute API'
+
+@app.route('/something', methods=['GET'])
+def something():
+    return str(1)
+
+@app.route('/getdata', methods=['GET'])
+def get_data():
+    return database
+
+# api/adddata?name=id
+@app.route('/adddata', methods=['GET', 'PUT'])
+def add_data():
+    key, value = list(request.agrs.items())[0]
+    database[key] = value
+    
+    print(key, value)
+    return f'{key} added'
+
+# api/deletedata?user=name
+@app.route('/deletedata', methods=['GET', 'DELETE'])
+def delete_data():
+    pass
+    
+if __name__ == '__main__':
+    app.run()
