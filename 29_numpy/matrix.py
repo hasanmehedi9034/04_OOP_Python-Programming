a341782612e6ec28be468d86a6e965c1ebcbb63e
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-24 02:30:06 +0600

    some operation in numpy

---
+++ 29_numpy/matrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+one_d = np.array([1, 2, 3])
+two_d = np.array([[1, 2], [3, 4], [5, 6]])
+tree_d = np.array([
+    [[1, 2], [3, 4], [5, 6]],
+    [[1, 2], [3, 4], [5, 6]]
+])
+
+print(tree_d)
+
+
