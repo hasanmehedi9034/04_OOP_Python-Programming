b00d31f7e31813e7dec85a7e6bb1a13321376544
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-24 01:52:20 +0600

    predict price

---
+++ 28_machine_learning/predict_price.py
@@ -0,0 +1,24 @@
+import pandas
+from sklearn.linear_model import LinearRegression
+
+data = pandas.read_csv('iphone_price.csv')
+model = LinearRegression()
+model.fit(data[['version']], data[['price']])
+predicted_price = model.predict([[15]])
+print(predicted_price)
+
+# install scikit-learn library
+
+
+
+
+
+
+
+
+
+
+
+
+
+
