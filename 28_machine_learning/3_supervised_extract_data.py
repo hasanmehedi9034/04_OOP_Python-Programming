fced52cb8048c8bac753e772cad9ca7c7611398d
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-23 20:47:34 +0600

    price and version

--- 28_machine_learning/3_supervised_extract_data.py
+++ 28_machine_learning/3_supervised_extract_data.py
@@ -8,6 +8,7 @@
 
 import requests
 from bs4 import BeautifulSoup
+import re
 
 url = 'https://en.wikipedia.org/wiki/IPhone'
 text = requests.get(url).text.encode('utf8').decode('ascii', 'ignore')
@@ -18,8 +19,12 @@ rows = table.findAll('tr')[1 : ]
 for row in rows:
     data = row.findAll(['th', 'td'])
     try:
-        # print(data[0].a.text.split(' ')[1])
-        print(data[-1])
+        version_text = (data[0].a.text.split(' ')[1])
+        version = re.sub(r"\D", "", version_text)
+        
+        price_text = (data[-1].text.split('/')[-1])
+        price = re.sub(r"\D", "", price_text)
+        print(version, price)
     except:
         pass
 
