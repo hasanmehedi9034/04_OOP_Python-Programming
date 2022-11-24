37e31f148012e75858ab529634884e1bf8ae2b5f
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-24 12:07:12 +0600

    array dimension

--- 29_numpy/matrix.py
+++ 29_numpy/matrix.py
@@ -7,6 +7,13 @@ tree_d = np.array([
     [[1, 2], [3, 4], [5, 6]]
 ])
 
-print(tree_d)
+print(tree_d.ndim) # check dimension of the Array
+
+print(tree_d.shape) # check shpae of the array
+
+
+
+
+
 
 
