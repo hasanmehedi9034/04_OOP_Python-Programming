b3dc89273baf1c3f3dcd517a54c0c456d0c86418
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-24 13:31:23 +0600

    data process

--- 29_numpy/digit_detecting.py
+++ 29_numpy/digit_detecting.py
@@ -6,7 +6,11 @@ digits = load_digits()
 # print(digits.data.shape)
 # print(dir(digits))
 
-plt.gray()
-for i in range(0, 5):
-    plt.matshow(digits.images[i])
-    plt.show()
+# plt.gray()
+# for i in range(0, 5):
+#     plt.matshow(digits.images[i])
+#     plt.show()
+
+
+
+
