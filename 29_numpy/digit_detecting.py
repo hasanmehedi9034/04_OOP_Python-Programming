6b41349af515fc415c8f2fcac86d8a36ccfba16c
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-24 12:26:01 +0600

    matplob

---
+++ 29_numpy/digit_detecting.py
@@ -0,0 +1,12 @@
+from sklearn.datasets import load_digits
+import matplotlib.pyplot as plt
+
+digits = load_digits()
+
+# print(digits.data.shape)
+# print(dir(digits))
+
+plt.gray()
+for i in range(0, 5):
+    plt.matshow(digits.images[i])
+    plt.show()
---
+++ 29_numpy/unsupervised.py
@@ -0,0 +1,11 @@
+'''
+    scikit learn (google)
+'''
+
+
+
+
+
+
+
+
