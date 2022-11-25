d19403c765a1c6ef1831cb0911a79ece7721eff4
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-25 11:23:09 +0600

    replace data

--- 30_lav_class_08/main.py
+++ 30_lav_class_08/main.py
@@ -1,11 +1,27 @@
-import pandas
+import pandas as pd
 
-data = pandas.read_csv('./HR_comma_sep.csv')
+data = pd.read_csv('./HR_comma_sep.csv')
 
 
 # step 1: missing data for any row any column
 # print(data.isnull().values.any())
 
 # step 2: check data type 
-print(data.dtypes)
+# print(data.dtypes)
+
+# step 3: check unique values
+# print(data.salary.unique())
+# print(data.Department.unique())
+
+clean_up_values = {
+    'salary': {
+        'low': 1,
+        'medium': 2,
+        'high': 3
+    }
+}
+
+data.replace(clean_up_values, inplace=True)
+print(data)
+
 
