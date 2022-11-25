2cd6c91092edc53eb89316e7a6b4810864f1b46b
hasanmehedi9034 <mehedi90344@gmail.com>
2022-11-25 11:39:11 +0600

    columns add and delete

--- 30_lav_class_08/main.py
+++ 30_lav_class_08/main.py
@@ -22,6 +22,23 @@ clean_up_values = {
 }
 
 data.replace(clean_up_values, inplace=True)
-print(data)
+# print(data)
+
+'''
+pandas get dummeis(google)
+'''
+
+# step 4: get dummies for the Department
+
+dummies = pd.get_dummies(data.Department)
+# print(dummies)
+
+# step 5: merge the dummies (dummy column) with original data
+merged = pd.concat([data, dummies], axis='columns')
+# print(merged)
+
+# step 6: drop unnecessary columns
+final_data = merged.drop(['Department', 'technical'], axis='columns')
+print(final_data)
 
 
