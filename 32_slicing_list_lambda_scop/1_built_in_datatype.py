15a3d0b10b1694eb2dcfdadc0e082bb418ec934c
hasanmehedi9034 <mehedi90344@gmail.com>
2022-12-02 00:29:51 +0600

    python data type

---
+++ 32_slicing_list_lambda_scop/1_built_in_datatype.py
@@ -0,0 +1,16 @@
+'''
+int, float, string, tuple, list, dictionary, set
+'''
+
+list1 = [1, 3, 4]
+
+lst = [1, 3, 4, [5, 6, [7, 8, [9, 10, [11, 12]]]]]
+lst[3][2][2][2].append(13)
+
+# print(lst[3][2][2][2])
+
+small_list = [1, 2, 3, 4]
+print(small_list[: : -1])
+
+
+
