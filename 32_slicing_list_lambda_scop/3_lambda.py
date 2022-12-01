2d2fb84d45ba4cf99280933c35ba18a6aa355ac1
hasanmehedi9034 <mehedi90344@gmail.com>
2022-12-02 01:14:59 +0600

    lambda function

---
+++ 32_slicing_list_lambda_scop/3_lambda.py
@@ -0,0 +1,39 @@
+def greet():
+    print('Good Morning')
+
+'''
+a = lambda arg: expression
+'''
+
+a = lambda : print('Hello Good Morning')
+
+s = 'Phitron'
+new_s = lambda st: print(st.upper()[: : -1])
+
+mx = lambda a, b: a if(a > b) else b
+
+lst = [1, 2, 3]
+new_lst = [lambda arg = i: arg * 2 for i in lst]
+# print(new_lst)
+
+my_list = [1, 2, 3, 4, 5, 6, 7]
+new_lst = list(filter(lambda x: (x % 2 == 0), my_list))
+# print(new_lst)
+
+string_list = ['hello', 'world', 'python']
+new_s = list(map(lambda x : x.upper(), string_list))
+# print(new_s)
+
+from functools import reduce
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+sum = reduce(lambda x, y: x + y, lst)
+print(sum)
+
+
+
+
+
+
+
+
+
