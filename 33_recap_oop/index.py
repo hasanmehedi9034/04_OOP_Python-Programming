b7dffb3725e3ee1ff2053fe51230bb927b98fb10
hasanmehedi9034 <mehedi90344@gmail.com>
2022-12-02 16:08:54 +0600

    class method, static method, abstract class and method

---
+++ 33_recap_oop/index.py
@@ -0,0 +1,43 @@
+# class School:
+#     school_name = 'ABC School' # class variable
+    
+#     def __init__(self, name):
+#         self.name = name
+        
+#     def print_name(self):
+#         print(self.name)
+        
+#     def change_name(self, name):
+#         self.school_name = name
+        
+#     @classmethod
+#     def change_school_name(cls):
+#         cls.school_name = 'ef School'
+        
+#     @staticmethod
+#     def greet():
+#         print("Hello Students")
+        
+# s1 = School('Mehedi')
+# s2 = School('rahat')
+
+
+from abc import ABC, abstractmethod
+class Vehicle(ABC):
+    def go():
+        pass
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
