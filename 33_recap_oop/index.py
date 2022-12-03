afa1da8e196a272c83458633ce28a90cc0fde09b
hasanmehedi9034 <mehedi90344@gmail.com>
2022-12-03 11:18:19 +0600

    basic curd operation

--- 33_recap_oop/index.py
+++ 33_recap_oop/index.py
@@ -1,43 +1,53 @@
-# class School:
-#     school_name = 'ABC School' # class variable
-    
-#     def __init__(self, name):
-#         self.name = name
-        
-#     def print_name(self):
-#         print(self.name)
-        
-#     def change_name(self, name):
-#         self.school_name = name
-        
-#     @classmethod
-#     def change_school_name(cls):
-#         cls.school_name = 'ef School'
-        
-#     @staticmethod
-#     def greet():
-#         print("Hello Students")
-        
-# s1 = School('Mehedi')
-# s2 = School('rahat')
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+students =[
+    {'id': 1,
+     'name': 'Rahim'
+    },
+    {'id': 2,
+     'name': 'Karim'
+    }
+]
+
+@app.route("/")
+def hello():
+    return jsonify(students)
+
+@app.route("/add", methods=['GET', "POST"])
+def add():
+    students.append(request.args)
+    return 'Student added succesfully'
+
+@app.route('/update', methods=['PUT', 'GET'])
+def update():
+    for student in students:
+        if str(student.get('id')) == request.args.get('id'):
+            student.update(request.args)
+    return 'Student updated succesfully'
+
+@app.route('/delete', methods=[ 'GET', 'DELETE'])
+def delete():
+    for i in range(len(students)):
+        if str(students[i].get('id')) == request.args.get('id'):
+            del students[i]
+            break
+    return 'student deleted succesfully'
+
+app.run(debug=True)
 
 
-from abc import ABC, abstractmethod
-class Vehicle(ABC):
-    def go():
-        pass
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
